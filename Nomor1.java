09888138b7ee0d11cd9ab1459ff4168d94c73c47
andiahmadnurmadani <106458721+andiahmadnurmadani@users.noreply.github.com>
2024-10-24 12:04:12 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Nomor1{
     public static void main(String[] args){
+      int honorLembur =0, jam, i;
 
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Total Jam lembur : ");
+      jam = scanner.nextInt();
+
+      for ( i = 1; i <= jam;i++){
+        if (i >= 16) {
+          jam-=16;
+
+          if (jam > 0) {
+            for (int j = 0; j < jam;j++)  {
+              honorLembur += 15000;
+            }
+          }
+        }
+        honorLembur+= 10000;
+        }
+      System.out.println("Honor lembur : "+ honorLembur);
     }
 }
